cdd619e3783683b9d9b61204ba223888173c8d35
arinZONE <arinzonenth@gmail.com>
2019-02-25 14:46:28 +0530

    load csv files using pandas

--- amazon-ses-csv-input.py
+++ amazon-ses-csv-input.py
@@ -1,61 +1,51 @@
 import boto3
 from botocore.exceptions import ClientError
 import csv
+import pandas
+import os
 count =0
+dirName ='/home/arinzone/.aws'
 # open the sender csv file for sender email ids with respective key and id 
-with open('em_sender_credentials_aws.csv') as File:
-  reader = csv.reader(File, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
-  header = next(reader)
-  for row in reader:
-    sender_email_ids=row[0]
-    print sender_email_ids
-    sender_email_ids=row[0]
-    aws_access_id=row[1]
-    aws_key_id=row[2]
-    email_accounts_limit=row[3]
-    print aws_access_id
-    print aws_key_id
-    # writes the key and id to /.aws/credentials file
-    key= open("/home/arinzone/.aws/credentials", "w")
-    key.write('[default]\naws_access_key_id = '+aws_access_id+'\naws_secret_access_key = '+aws_key_id)
+send = pandas.read_csv('em_sender_credentials_aws.csv',names=['sender_email_ids','aws_access_id','aws_key_id','email_accounts_limit'])
+print send['sender_email_ids'][1]
+print send['aws_access_id'][1]
+print send['aws_key_id'][1]
+print send['email_accounts_limit'][1]
+# writes the key and id to /.aws/credentials file
+if not os.path.exists(dirName):
+    os.makedirs(dirName)
+with open("/home/arinzone/.aws/credentials", "w") as key:
+    key.write('[default]\naws_access_key_id = '+send['aws_access_id'][1]+'\naws_secret_access_key = '+send['aws_key_id'][1])
     key.close()
-    # open the receiver's csv file for email ids and names
-    with open('em_recipient_email_ids.csv') as File2:
-      reader = csv.reader(File2, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
-      header = next(reader)
-      for row in reader:
-        receiver_email_ids=row[0]
-        print email_accounts_limit
-        print "\t"+receiver_email_ids
-        with open('em_email_body.csv') as File3:
-            reader = csv.reader(File3,delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
-            header = next(reader)
-            for row in reader:
-                body_subject=row[0]
-                file_path=row[1]
-                cc=row[2]
-                bcc=row[3]
-                email_body=row[4]
-                print body_subject
-                # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-                AWS_REGION = "us-west-2"
-                # Create a new SES resource and specify a region.
-                client = boto3.client('ses',region_name=AWS_REGION)
-                # Try to send the email.
-                try:
-                    count+=1
-                    print count
-                    #Provide the contents of the email.
-                    response = client.send_raw_email(
-                    RawMessage={
-                        'Data': 'From: '+sender_email_ids+'\nTo: '+receiver_email_ids+'\nSubject:'+body_subject+'\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n'+email_body+'\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename=\"'+file_path+'\"\n\nAttachment:\n\n--NextPart--',
-                    }
-                    #ConfigurationSetName='string'
-                )
-                # Display an error if something goes wrong. 
-                except ClientError as e:
-                    print(e.response['Error']['Message'])
-                else:
-                    print("Email sent! Message ID:"),
-                    print(response['MessageId'])
+# open the receiver's csv file for email ids and names
+rece = pandas.read_csv('em_recipient_email_ids.csv',names=['recipient_email_ids','recipient_first_name','recipient_middle_name','recipient_last_name'])
+print send['email_accounts_limit']
+print "\t"+rece['recipient_email_ids'][1]
+body = pandas.read_csv('em_email_body.csv',names=['body_subject','body_filepath','body_cc','body_bcc','body_text'])
+print body['body_subject'][1]
+# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "us-west-2"
+# Create a new SES resource and specify a region.
+client = boto3.client('ses',region_name=AWS_REGION)
+# Try to send the email.
+try:
+    count+=1
+    print count
+    #Provide the contents of the email.
+    response = client.send_raw_email(
+    RawMessage={
+        'Data': 'From: '+send['sender_email_ids'][1]+'\nTo: '
+        +rece['recipient_email_ids'][1]+'\nSubject:'+body['body_subject'][1]
+        +'\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n'
+        +body['body_text'][1]+'\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename=\"'
+        +body['body_filepath'][1]+'\"\n\nAttachment:\n\n--NextPart--',
+    }
+    #ConfigurationSetName='string'
+)
+# Display an error if something goes wrong. 
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
 
